36fbbf0242a559f2a94c561ac876ec59d3c79545
joellsalvi <joellsalvi@gmail.com>
2018-07-17 15:33:35 -0300

    DbConfig

--- js-middleware-data-access/src/main/java/br/com/middleware/dataaccess/config/DbConfig.java
+++ js-middleware-data-access/src/main/java/br/com/middleware/dataaccess/config/DbConfig.java
@@ -1,12 +1,9 @@
 package br.com.middleware.dataaccess.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -25,110 +22,4 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan("br.com.middleware.dataaccess.entity")
 public class DbConfig {
 
-    @Autowired
-    private Environment environment;
-
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-    /**
-     * HEROKU DATASOURCE
-     *
-     * @return
-     */
-//    @Bean
-//    public DataSource dataSource() {
-//        if (dbUrl == null || dbUrl.isEmpty()) {
-//            HikariDataSource ds = new HikariDataSource();
-//            ds.setDriverClassName(environment.getRequiredProperty("datasource.driver.classname"));
-//            ds.setJdbcUrl(getJdbcUrl());
-//            ds.setUsername(environment.getRequiredProperty("datasource.username"));
-//            ds.setPassword(environment.getRequiredProperty("datasource.password"));
-//            ds.setMaximumPoolSize(Integer.valueOf(environment.getRequiredProperty("datasource.pool.maxSize")));
-//            return ds;
-//        } else {
-//            HikariConfig config = new HikariConfig();
-//            config.setJdbcUrl(dbUrl);
-//            return new HikariDataSource(config);
-//        }
-//    }
-
-    /**
-     * LOCAL DATASOURCE
-     *
-     * @return
-     */
-//    @Bean
-//    public DataSource dataSource() {
-//        try {
-//            HikariDataSource ds = new HikariDataSource();
-//            ds.setDriverClassName(environment.getRequiredProperty("datasource.driver.classname"));
-//            ds.setJdbcUrl(getJdbcUrl());
-//            ds.setUsername(environment.getRequiredProperty("datasource.username"));
-//            ds.setPassword(environment.getRequiredProperty("datasource.password"));
-//            ds.setMaximumPoolSize(Integer.valueOf(environment.getRequiredProperty("datasource.pool.maxSize")));
-//            return ds;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
-//    @Bean
-//    public JpaTransactionManager transactionManager() {
-//        JpaTransactionManager transactionManager = new JpaTransactionManager();
-//        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-//        return transactionManager;
-//    }
-//
-//    @Bean
-//    @DependsOn("flyway")
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-////        entityManagerFactoryBean.setDataSource(dataSource());
-//        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-//        entityManagerFactoryBean.setPackagesToScan(new String[]{"br.com.middleware.dataaccess.entity"});
-////        entityManagerFactoryBean.setJpaProperties(hibernateProperties());
-//        return entityManagerFactoryBean;
-//    }
-
-//    private Properties hibernateProperties() {
-//        Properties properties = new Properties();
-//        properties.put("hibernate.default_schema", getDefaultSchema());
-//        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-//        properties.put("hibernate.show_sql", environment.getRequiredProperty("spring.jpa.show-sql"));
-//        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-//        return properties;
-//    }
-
-//    private String getJdbcUrl() {
-//        String url = environment.getRequiredProperty("datasource.url");
-//        String appender = "";
-//
-//        String schema = getDefaultSchema();
-//        if (!url.contains("currentSchema") && !StringUtils.isBlank(schema)) {
-//            appender += "currentSchema=" + schema;
-//        }
-//
-//        Boolean sslmode = getSslMode();
-//        if (sslmode) {
-//            if(url.contains("currentSchema")) {
-//                appender += "&";
-//            }
-//            appender += "sslmode=" + sslmode;
-//        }
-//
-//        if(!url.contains("?") && StringUtils.isNotBlank(appender)) {
-//            appender = "?".concat(appender);
-//        }
-//
-//        return url.concat(appender);
-//    }
-//
-//    private String getDefaultSchema() {
-//        return environment.getRequiredProperty("spring.jpa.properties.hibernate.default_schema");
-//    }
-//
-//    private Boolean getSslMode() {
-//        return Boolean.valueOf(environment.getRequiredProperty("datasource.sslmode"));
-//    }
 }
